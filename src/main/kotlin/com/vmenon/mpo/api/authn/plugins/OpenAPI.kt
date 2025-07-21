21ba783cdf78252cb03160b1cd1a8fc3b03a9d43
Vinayak Menon <vviswana@gmail.com>
2025-07-21 17:15:43 -0500

    Fixing build

--- src/main/kotlin/com/vmenon/mpo/api/authn/plugins/OpenAPI.kt
+++ src/main/kotlin/com/vmenon/mpo/api/authn/plugins/OpenAPI.kt
@@ -1,16 +1,13 @@
 package com.vmenon.mpo.api.authn.plugins
 
-import io.ktor.server.application.*
-import io.ktor.server.plugins.openapi.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.Application
+import io.ktor.server.plugins.openapi.openAPI
+import io.ktor.server.plugins.swagger.swaggerUI
+import io.ktor.server.routing.routing
 
 fun Application.configureOpenAPI() {
     routing {
-        openAPI(path = "openapi", swaggerFile = "openapi/documentation.yaml") {
-            codeSamples = true
-        }
-
+        openAPI(path = "openapi", swaggerFile = "openapi/documentation.yaml")
         swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml") {
             version = "4.15.5"
         }
